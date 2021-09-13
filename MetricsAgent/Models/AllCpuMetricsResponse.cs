006b64ce79645c2e4e216ba706574d725d701530
Artem <a.khimin@yandex.ru>
2021-09-13 13:03:34 +0300

    Сделал MetricsAgent all

---
+++ MetricsAgent/Models/AllCpuMetricsResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Models
+{
+    public class AllCpuMetricsResponse
+    {
+        public List<CpuMetricDto> Metrics { get; set; }
+
+    }
+}
---
+++ MetricsAgent/Models/ICpuMetricsRepository .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Models
+{
+    // маркировочный интерфейс
+    // необходим, чтобы проверить работу репозитория на тесте-заглушке
+    public interface ICpuMetricsRepository : IRepository<CpuMetric>
+    {
+
+    }
+
+}
